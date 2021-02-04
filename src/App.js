5d40ba32a1ccd07fe9c32397d8513ce0b8a4bb15
nejmiyassine01 <nejmi.yassine01@gmail.com>
2021-02-04 18:40:08 +0100

    Update menu-items withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage.pages";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+	<div>
+		<h1>HATS PAGE</h1>
+	</div>
+);
 
 function App() {
 	return (
 		<div className="app">
-			<Homepage />
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route path="/hats" component={HatsPage} />
+			</Switch>
 		</div>
 	);
 }
